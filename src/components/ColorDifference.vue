a2efc70f70c61944d675942db837aa3ccc9c0d27
iwouldlike2rage <iwouldlike2rage@gmail.com>
2019-03-11 20:52:41 -0400

    Styling and filtering

--- src/components/ColorDifference.vue
+++ src/components/ColorDifference.vue
@@ -1,23 +1,46 @@
 <template>
     <div>
-        <table>
-            <tbody>
-            <tr v-for="c in vmc" :key="c.id">
-                <td>
-                    <div class="color">
-                        <div class="swatch" :style="`background-color: ${c.rgb}`"></div>
-                        <div><span class="name">{{ c.name }}</span><br><span class="family">{{c.family}}</span><br><span class="code">{{c.code}}</span><br><span>&nbsp;</span></div>
-                    </div>
-                </td>
-                <td v-for="s in c.scores.slice(0, 5)" :key="s.d">
-                    <div class="color">
-                        <div class="swatch" :style="`background-color: ${s.c.rgb}`"></div>
-                        <div><span class="name">{{ s.c.name }}</span><br><span class="family">{{s.c.family}}</span><br><span class="code">{{s.c.code}}</span><br><span class="score">{{numeral(s.d).format('0.00')}}</span></div>
-                    </div>
-                </td>
-            </tr>
-            </tbody>
-        </table>
+        <div class="form">
+            <input type="text" v-model="textFilter" placeholder="Search colors">
+        </div>
+        <hr>
+        <div>
+            <table>
+                <thead>
+                <tr>
+                    <th>Base color</th>
+                    <th colspan="5">Matches</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="c in filteredColors" :key="c.id">
+                    <td>
+                        <div class="color">
+                            <div class="swatch" :style="`background-color: ${c.rgb}`"></div>
+                            <div><span class="name">{{ c.name }}</span><br><span
+                                    class="family">{{c.family}}</span><br><span
+                                    class="code">{{c.code}}</span></div>
+                        </div>
+                    </td>
+                    <td v-for="s in c.scores.slice(0, 5)" :key="`${c.code}-${s.c.code}`">
+                        <div class="color">
+                            <div class="swatch" :style="`background-color: ${s.c.rgb}`">
+                                <div class="score">
+                                    <span class="badge"
+                                          :class="{ success: s.d < 2, warning: s.d >=2 && s.d < 10, danger: s.d >=10 }">
+                                        {{numeral(s.d).format('0.00')}}
+                                    </span></div>
+                            </div>
+                            <div><span class="name">{{ s.c.name }}</span><br><span
+                                    class="family">{{s.c.family}}</span><br><span
+                                    class="code">{{s.c.code}}</span><br>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -32,6 +55,7 @@
     export default {
         data() {
             return {
+                textFilter: '',
                 vmc: [],
                 vgc: [],
                 numeral,
@@ -40,7 +64,12 @@
         computed: {
             all() {
                 return [...this.vmc, ...this.vgc];
-            }
+            },
+            filteredColors() {
+                return this.textFilter.trim() !== ''
+                    ? this.all.filter(x => x.name.toLowerCase().indexOf(this.textFilter) > -1 || x.code.toLowerCase().indexOf(this.textFilter) > -1)
+                    : this.all;
+            },
         },
         mounted() {
             let id = 0;
@@ -61,7 +90,7 @@
                     b,
                     lab: {L: lab[0], A: lab[1], B: lab[2]}
                 };
-            }).sort((a,b) => a.name.localeCompare(b.name));
+            }).sort((a, b) => a.name.localeCompare(b.name));
             this.vgc = VallejoGameColors.map((c) => {
                 id += 1;
                 const r = parseInt(c.rgb.slice(1, 3), 16);
@@ -79,11 +108,13 @@
                     b,
                     lab: {L: lab[0], A: lab[1], B: lab[2]}
                 };
-            }).sort((a,b) => a.name.localeCompare(b.name));
+            }).sort((a, b) => a.name.localeCompare(b.name));
 
-            this.vmc.forEach((c1) => {
+            this.all.forEach((c1) => {
+                id += 1;
                 const scores = this.all.filter(c => c.id !== c1.id).map((c2) => {
                     return {
+                        id,
                         c: c2,
                         d: deltaE.getDeltaE00(c1.lab, c2.lab)
                     };
@@ -107,24 +138,97 @@
     .swatch {
         width: 64px;
         min-width: 64px;
-        height: 64px;cypress\fixtures\example.json
+        height: 64px;
         margin-right: 8px;
+        box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+        border: solid 1px rgba(0, 0, 0, 0.1);
     }
 
     .name {
         font-weight: 600;
     }
 
-    .family{
+    .family {
         font-size: 0.9em;
     }
-    .code{
+
+    .code {
         font-family: monospace;
     }
 
     .score {
-        font-family: monospace;
+        margin: 10px 0 0 6px;
+    }
+
+    .form {
+        margin-bottom: 16px;
+    }
+
+    input {
+        border: solid 1px #ccc;
+        border-radius: 4px;
+        padding: 6px 12px;
+        -webkit-box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+        -moz-box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+        box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+    }
+
+    table {
+        border-spacing: 0;
+    }
+
+    table thead th {
+        padding: 8px 16px;
+        background: #ccc;
+        font-size: 13px;
         font-weight: 600;
+        border-bottom: solid 1px rgba(0, 0, 0, 0.1);
+        box-shadow: 0 2px 3px rgba(0, 0, 0, 0.05);
+    }
+
+    table tbody tr:nth-of-type(2n) {
+        background: #fff;
+    }
+
+    table tbody tr:nth-of-type(2n+1) {
+        background: #eee;
+    }
+
+    table tbody tr td {
+        padding: 8px 8px;
+    }
+
+    table tbody tr td:first-of-type {
+        border-right: solid 1px #ccc;
+        background: rgba(0, 0, 0, 0.05);
+    }
+
+    table tbody tr td:nth-of-type(2) {
+        padding-left: 16px;
+    }
+
+    .badge {
+        padding: 3px 6px;
+        border-radius: 6px;
+        border: solid 1px rgba(255, 255, 255, .25);
+        box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+        font-weight: 800;
+        font-size: 10px;
+    }
+
+    .badge.success {
+        background: green;
+        color: #fff;
+    }
+
+    .badge.warning {
+        background: #ccc;
+        color: rgba(0, 0, 0, 0.65);
+    }
+
+    .badge.danger {
+        background: #ff0042;
+        color: #fff;
     }
 
 </style>
